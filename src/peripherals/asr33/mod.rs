60aae3b597cd18a28766c07a5414f544591c1a82
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-08-21 19:58:12 +0200

    refactor: compose ASR-33 peripheral submodules

--- src/peripherals/asr33/mod.rs
+++ src/peripherals/asr33/mod.rs
@@ -1,5 +1,10 @@
 mod answerback;
+mod keyboard;
+mod mechanics;
 mod model;
+mod paper_tape;
 
 pub use answerback::Answerback;
+pub use keyboard::*;
 pub use model::*;
+pub(crate) use mechanics::MechanicsState;
