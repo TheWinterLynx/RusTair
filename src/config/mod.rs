0d37c36c7ffbd5ca93e9886a701e739665bbc447
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-08-22 08:50:09 +0200

    Export emulation speed configuration

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,6 @@
 mod machine;
 
 pub use machine::{
-    AppConfig, Asr33Speed, CompatibilityConfig, CpuModel, MachineConfig, PeripheralConfig,
-    PreferencesConfig, RamInit, RamSize, SerialBoard, TerminalSpeed,
+    AppConfig, Asr33Speed, CompatibilityConfig, CpuModel, EmulationSpeed, MachineConfig,
+    PeripheralConfig, PreferencesConfig, RamInit, RamSize, SerialBoard, TerminalSpeed,
 };
