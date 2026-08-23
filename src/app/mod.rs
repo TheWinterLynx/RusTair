7fea11900fc0eb4c6eb0b3fd1ce462a73f025480
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-08-23 17:17:13 +0200

    Fix app constant after COM wiring edit [skip ci]

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -36,7 +36,7 @@ const TTY_H: f32 = teletype::IMAGE_H;
 const PANEL_FRAME: Duration = Duration::from_millis(16);
 const KEY_TAP_TIME: Duration = Duration::from_millis(50);
 const PRINT_HEAD_STRIKE_TIME: Duration = Duration::from_millis(84);
-const PRINT_HEAD_IMACT_DELAY: Duration = Duration::from_millis(20);
+const PRINT_HEAD_IMPACT_DELAY: Duration = Duration::from_millis(20);
 const PRINT_HEAD_CARRIAGE_RETURN_TIME: Duration = Duration::from_millis(160);
 const PAPER_FEED_TIME: Duration = Duration::from_millis(74);
 
