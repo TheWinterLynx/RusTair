0d1d0a223d4d6b19c1c93bb618c5b856ef080d44
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-08-23 11:32:26 +0200

    Export external serial character mode [skip ci]

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,9 @@
 mod external_serial;
 mod machine;
 
-pub use external_serial::{ExternalSerialConfig, ExternalSerialSpeed, TcpListenScope};
+pub use external_serial::{
+    ExternalSerialCharacterMode, ExternalSerialConfig, ExternalSerialSpeed, TcpListenScope,
+};
 pub use machine::{
     AppConfig, Asr33Speed, CompatibilityConfig, CpuModel, EmulationSpeed, MachineConfig,
     PeripheralConfig, PreferencesConfig, RamInit, RamSize, SerialBoard, TerminalSpeed,
