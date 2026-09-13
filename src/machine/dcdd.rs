a7b7ff6320b593053c9927ed5a30e146498d9977
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-09-13 11:56:50 +0200

    Restore DCDD non-decoded write test stimulus

--- src/machine/dcdd.rs
+++ src/machine/dcdd.rs
@@ -617,7 +617,7 @@ mod tests {
                 wds: true
             }
         );
-        board1.observe_s100(&io_sample(0x0b, false, true, false, true, 0x00));
+        board1.observe_s100(&io_sample(0x0b, false, true, false, false, 0x00));
         assert_eq!(
             board1_harness_drive(&harness),
             Board1HarnessDrive::default()
