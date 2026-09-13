27d1db2e6122cabae72eea9be8cbd3ccbc51c976
TheWinterLynx <64207335+TheWinterLynx@users.noreply.github.com>
2026-09-13 19:52:23 +0200

    fix(adm3a): open ADM-3A for embedded CPU diagnostics [skip ci]

--- src/app/embedded_cpu_diagnostics.rs
+++ src/app/embedded_cpu_diagnostics.rs
@@ -723,6 +723,7 @@ impl RusTairApp {
         match self.serial_router.device_on(connection) {
             Some(SerialDevice::InternalAsr33) => self.asr33.window_open = true,
             Some(SerialDevice::TextTerminal) => self.terminal.window_open = true,
+            Some(SerialDevice::Adm3a) => self.adm3a.window_open = true,
             Some(SerialDevice::ExternalTcp) => self.external_serial.window_open = true,
             Some(SerialDevice::ExternalCom) => self.external_com.window_open = true,
             None => {}
